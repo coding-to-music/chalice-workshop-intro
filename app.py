8d8bf440bbc0fe5efbaac224cbd1119caea446f4
coding-to-music <connors.tom@gmail.com>
2021-07-13 21:47:34 +0000

    initial

---
+++ app.py
@@ -0,0 +1,13 @@
+from chalice import Chalice
+
+app = Chalice(app_name='workshop-intro')
+
+@app.lambda_function()
+def hello_world(event, context):
+    return {'hello': 'world'}
+
+
+@app.lambda_function()
+def hello_name(event, context):
+    name = event['name']
+    return {'hello': name}
